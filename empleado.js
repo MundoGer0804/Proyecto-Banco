e33f20d4b0e77412fb4741e4c4bcecb043427389
Leserdemun <leserdemun@gmail.com>
2023-03-08 00:34:34 -0600

    Se_agrego_Empleado

---
+++ empleado.js
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Empleados</title>
+</head>
+<body>
+	<h1>Empleados del banco</h1>
+	<table>
+		<tr>
+			<th>Nombre</th>
+			<th>Cargo</th>
+			<th>Contacto</th>
+		</tr>
+		<tr>
+			<td><input EdmundoGer>
+			<td>Gerente General</td>
+			<td>EdmundoGer@gmail.com</td>
+		</tr>
+		<tr>
+			<td>Wendy</td>
+			<td>Cajera</td>
+			<td>Vittewendy@gmail.com</td>
+		</tr>
+		<tr>
+			<td>vacante</td>
+			<td>Analista de Crédito</td>
+			<td>vacante</td>
+		</tr>
+		<tr>
+			<td>vacante</td>
+			<td>Ejecutiva de Cuentas</td>
+			<td>vacante</td>
+		</tr>
+		<tr>
+			<td>vacante</td>
+			<td>Gerente de Finanzas</td>
+			<td>vacante</td>
+		</tr>
+	</table>
+</body>
+</html>
